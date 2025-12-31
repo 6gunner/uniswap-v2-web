d3f82dadc158c16516ec2db1d71fc25b9a61ec2c
coda <ky302511526@gmail.com>
2025-12-31 12:47:42 +0800

    update

--- build_shell/swap.sh
+++ build_shell/swap.sh
@@ -4,15 +4,15 @@ if [ "$CF_PAGES_BRANCH" == "main" ]; then
   # Run the "production" script in `package.json` on the "production" branch
   # "production" should be replaced with the name of your Production branch
 
-  pnpm run build:production --filter=web
+  pnpm run build:production --filter=swap
 
 elif [ "$CF_PAGES_BRANCH" == "preview" ]; then
   # Run the "staging" script in `package.json` on the "staging" branch
   # "staging" should be replaced with the name of your specific branch
 
-  pnpm run build:production --filter=web
+  pnpm run build:production --filter=swap
 
 else
   # Else run the dev script
-  pnpm run build:testing --filter=web
+  pnpm run build:testing --filter=swap
 fi
